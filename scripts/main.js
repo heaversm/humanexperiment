3cd8d23c16118e67f932719b73713a5a73c5a202
Mike Heavers <mheavers@gmail.com>
2014-01-02 14:27:43 -0500

    add click functionality to menu when iconized (for touch devices)

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,20 @@ var site =  (function (window,document, $){
     }
     function addListeners(){
         addResizeListener();
+        addNavListener();
     }
     function addResizeListener(){
         $(window).resize(function() {
             aspectResize();
         });
+    }
+    function addNavListener(){
+        $('#top-nav').click(function(e){
+            var $el = $(this);
+            if (document.documentElement.clientWidth < 960) {
+                $el.toggleClass('active');
+            }
+        })
     }
     function aspectResize(){
         $('.aspect-resize').each(function(){
